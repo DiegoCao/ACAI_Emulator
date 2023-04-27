e6a5cd64061cd347723312ed250a903970df7f6f
Qiu Yuqing <qyuqing@umich.edu>
2023-04-27 02:49:40 -0400

    add edge yolo inference

---
+++ YOLOv3_Train_Inference/edge_yolo_inference.py
@@ -0,0 +1,87 @@
+import torch
+import time
+import shutil
+import os
+import random
+from model import *
+from dataset import *
+from utils import *
+
+def comm_with_cloud(samples):
+    return 'yolo_detector.pt'
+
+
+# get dataset
+val_dataset = get_pascal_voc2007_data('content', 'val')
+inference_dataset = filter_dataset_with_class(val_dataset, 'cat', 1)
+inference_loader = pascal_voc2007_loader(inference_dataset, 1)
+
+# load pre-trained model
+detector = SingleStageDetector()
+detector.load_state_dict(torch.load('yolo_detector.pt', map_location=torch.device('cpu')))
+
+# inference images
+thresh=0.8
+nms_thresh=0.3
+output_dir='mAP/input'
+incorrect_thresh = 10
+incorrect_samples = []
+
+detector.eval()
+start_t = time.time()
+
+if output_dir is not None:
+    det_dir = 'mAP/input/detection-results'
+    gt_dir = 'mAP/input/ground-truth'
+    if os.path.exists(det_dir):
+        shutil.rmtree(det_dir)
+    os.mkdir(det_dir)
+    if os.path.exists(gt_dir):
+        shutil.rmtree(gt_dir)
+    os.mkdir(gt_dir)
+
+for iter_num, data_batch in enumerate(inference_loader):
+    images, boxes, w_batch, h_batch, img_ids = data_batch
+    final_proposals, final_conf_scores, final_class = detector.inference(images, thresh=thresh, nms_thresh=nms_thresh)
+
+    # clamp on the proposal coordinates
+    batch_size = len(images)
+    for idx in range(batch_size):
+        torch.clamp_(final_proposals[idx][:, 0::2], min=0, max=w_batch[idx])
+        torch.clamp_(final_proposals[idx][:, 1::2], min=0, max=h_batch[idx])
+        img = images[idx]
+        valid_box = sum([1 if j != -1 else 0 for j in boxes[idx][:, 0]])
+        final_all = torch.cat((final_proposals[idx], \
+                               final_class[idx].float(), final_conf_scores[idx]), dim=-1).cpu()
+        resized_proposals = coord_trans(final_all, w_batch[idx], h_batch[idx])
+
+        # write results to file for evaluation (use mAP API https://github.com/Cartucho/mAP for now...)
+        if output_dir is not None:
+            file_name = img_ids[idx].replace('.jpg', '.txt')
+            with open(os.path.join(det_dir, file_name), 'w') as f_det, \
+                open(os.path.join(gt_dir, file_name), 'w') as f_gt:
+                print('{}: {} GT bboxes and {} proposals'.format(img_ids[idx], valid_box, resized_proposals.shape[0]))
+                for b in boxes[idx][:valid_box]:
+                    f_gt.write('{} {:.2f} {:.2f} {:.2f} {:.2f}\n'.format(idx_to_class[b[4].item()], b[0], b[1], b[2], b[3]))
+                for b in resized_proposals:
+                    f_det.write('{} {:.6f} {:.2f} {:.2f} {:.2f} {:.2f}\n'.format(idx_to_class[b[4].item()], b[5], b[0], b[1], b[2], b[3]))
+        else:
+            data_visualizer(img, idx_to_class, boxes[idx][:valid_box], resized_proposals)
+        
+        # check if the sample is incorrect
+        detected_classes = [idx_to_class[b[4].item()] for b in boxes[idx][:valid_box]].sort()
+        gt_classes = [idx_to_class[b[4].item()] for b in resized_proposals].sort()
+        if detected_classes != gt_classes:
+            incorrect_samples.append((images[idx], boxes[idx], w_batch[idx], h_batch[idx], img_ids[idx]))
+
+            # communicate with cloud to get new model checkpoint
+            if len(incorrect_samples) == incorrect_thresh:
+                new_model_pt = comm_with_cloud(incorrect_samples)
+                detector.load_state_dict(torch.load(new_model_pt, map_location=torch.device('cpu')))
+                incorrect_samples = []
+
+    # sleep between inference requests
+    time.sleep(random.randint(1, 10) / 10)
+
+end_t = time.time()
+print('Total inference time: {:.1f}s'.format(end_t-start_t))
---
+++ YOLOv3_Train_Inference/utils.py
@@ -0,0 +1,97 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+def coord_trans(bbox, w_pixel, h_pixel, w_amap=7, h_amap=7, mode='a2p'):
+    """
+    Coordinate transformation function. It converts the box coordinate from
+    the image coordinate system to the activation map coordinate system and vice versa.
+    In our case, the input image will have a few hundred pixels in
+    width/height while the activation map is of size 7x7.
+
+    Input:
+    - bbox: Could be either bbox, anchor, or proposal, of shape Bx*x4
+    - w_pixel: Number of pixels in the width side of the original image, of shape B
+    - h_pixel: Number of pixels in the height side of the original image, of shape B
+    - w_amap: Number of pixels in the width side of the activation map, scalar
+    - h_amap: Number of pixels in the height side of the activation map, scalar
+    - mode: Whether transfer from the original image to activation map ('p2a') or
+          the opposite ('a2p')
+
+    Output:
+    - resized_bbox: Resized box coordinates, of the same shape as the input bbox
+    """
+
+    assert mode in ('p2a', 'a2p'), 'invalid coordinate transformation mode!'
+    assert bbox.shape[-1] >= 4, 'the transformation is applied to the first 4 values of dim -1'
+
+    if bbox.shape[0] == 0: # corner cases
+        return bbox
+
+    resized_bbox = bbox.clone()
+    # could still work if the first dim of bbox is not batch size
+    # in that case, w_pixel and h_pixel will be scalars
+    resized_bbox = resized_bbox.view(bbox.shape[0], -1, bbox.shape[-1])
+    invalid_bbox_mask = (resized_bbox == -1) # indicating invalid bbox
+
+    if mode == 'p2a':
+        # pixel to activation
+        width_ratio = w_pixel * 1. / w_amap
+        height_ratio = h_pixel * 1. / h_amap
+        resized_bbox[:, :, [0, 2]] /= width_ratio.view(-1, 1, 1)
+        resized_bbox[:, :, [1, 3]] /= height_ratio.view(-1, 1, 1)
+    else:
+        # activation to pixel
+        width_ratio = w_pixel * 1. / w_amap
+        height_ratio = h_pixel * 1. / h_amap
+        resized_bbox[:, :, [0, 2]] *= width_ratio.view(-1, 1, 1)
+        resized_bbox[:, :, [1, 3]] *= height_ratio.view(-1, 1, 1)
+
+    resized_bbox.masked_fill_(invalid_bbox_mask, -1)
+    resized_bbox.resize_as_(bbox)
+    return resized_bbox
+
+def data_visualizer(img, idx_to_class, bbox=None, pred=None):
+    """
+    Data visualizer on the original image. Support both GT box input and proposal 
+    input.
+
+    Input:
+    - img: PIL Image input
+    - idx_to_class: Mapping from the index (0-19) to the class name
+    - bbox: GT bbox (in red, optional), a tensor of shape Nx5, where N is
+          the number of GT boxes, 5 indicates (x_tl, y_tl, x_br, y_br, class)
+    - pred: Predicted bbox (in green, optional), a tensor of shape N'x6, where
+          N' is the number of predicted boxes, 6 indicates
+          (x_tl, y_tl, x_br, y_br, class, object confidence score)
+    """
+
+    img_copy = np.array(img).astype('uint8')
+
+    if bbox is not None:
+        for bbox_idx in range(bbox.shape[0]):
+            one_bbox = bbox[bbox_idx][:4]
+            cv2.rectangle(img_copy, (int(one_bbox[0]), int(one_bbox[1])), (int(one_bbox[2]),
+                      int(one_bbox[3])), (255, 0, 0), 2)
+            if bbox.shape[1] > 4: # if class info provided
+                obj_cls = idx_to_class[bbox[bbox_idx][4].item()]
+                cv2.putText(img_copy, '%s' % (obj_cls),
+                          (one_bbox[0], one_bbox[1]+15),
+                          cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 255), thickness=1)
+
+    if pred is not None:
+        for bbox_idx in range(pred.shape[0]):
+            one_bbox = pred[bbox_idx][:4]
+            cv2.rectangle(img_copy, (int(one_bbox[0]), int(one_bbox[1])), (int(one_bbox[2]),
+                      int(one_bbox[3])), (0, 255, 0), 2)
+
+            if pred.shape[1] > 4: # if class and conf score info provided
+                obj_cls = idx_to_class[pred[bbox_idx][4].item()]
+                conf_score = pred[bbox_idx][5].item()
+                cv2.putText(img_copy, '%s, %.2f' % (obj_cls, conf_score),
+                            (one_bbox[0], one_bbox[1]+15),
+                            cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 255), thickness=1)
+
+    plt.imshow(img_copy)
+    plt.axis('off')
+    plt.show()

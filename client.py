4711da93024bba27c43c03eef7d941730be6e025
DiegoCao <945003692@qq.com>
2023-04-29 15:10:30 -0400

    sample communication

--- client.py
+++ client.py
@@ -1,32 +1,49 @@
 '''
 Simple example of the client.py
 '''
-
 import socket
 import pickle
-
-
+import torch
+    
 HEADERSIZE = 10
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1243))
+s.connect((socket.gethostname(), 1234))
 image_counter = 0
 
-while True:
-    full_msg = b''
-    new_msg = True
-    while True:
-        msg = s.recv(16)
-        if new_msg:
-            print("new msg len:",msg[:HEADERSIZE])
-            msglen = int(msg[:HEADERSIZE])
-            new_msg = False
 
-        print(f"full message length: {msglen}")
-        full_msg += msg
-        if len(full_msg)-HEADERSIZE == msglen:
-            file_write_path = "client_" + str(image_counter) + ".jpg"
-            recv_img = open(file_write_path, "wb")
-            recv_img.write(full_msg[HEADERSIZE:])
-            recv_img.close()
-            new_msg = True
-            full_msg = b""
+def clientSendImg(s, imgdir):
+    for image_path in imagedir:
+        img = util.encodeBytes(image_path)
+        s.sendall(image_data)
+
+        time.sleep(500)
+
+ack_msg = "ack"
+pt_save_path = "tmp.pt"
+print("[STARTING] Client 1 wakes up")
+print("Client 1 starts handling requests")
+def client_logic():
+    image_counter = 0
+    while True:
+        print("start receiving message")
+        full_msg = b''
+        new_msg = True
+        while True:
+            msg = s.recv(1024)
+            if new_msg:
+                print("new msg len:",msg[:HEADERSIZE])
+                msglen = int(msg[:HEADERSIZE])
+                new_msg = False
+            # print(f"full message length: {msglen}")
+            full_msg += msg
+            # print("the length of fullmessage is ", len(full_msg)) # need to get wait
+            if len(full_msg)-HEADERSIZE == msglen:
+                obj = pickle.loads(full_msg[HEADERSIZE:])
+                print(obj)
+                # recv_wt = open(file_write_path, "wb")
+                recv_wt.write(full_msg[HEADERSIZE:])
+                recv_wt.close()
+                new_msg = True
+                full_msg = b""
+
+client_logic()
--- server.py
+++ server.py
@@ -3,21 +3,63 @@ Simple example of the server.py
 '''
 import socket
 import time
+import torch
+import threading
 import pickle
 import util
+import time
 
-HEADERSIZE = 10
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1243))
-s.listen(5)
+def serverSendweight(clientsocket, filepath):
+    weights = util.encodeBytes(filepath)
+    msg = util.imgTobytes(weights)
+    clientsocket.sendall(msg)
+    
+    
 
-while True:
-    # now our endpoint knows about the OTHER endpoint.
-    clientsocket, address = s.accept()
+# def serverRecvimgs(clientsocket):
+#     image_list = []
+#     while True:
+#         msg = 
+
+    
+
+
+def handle_client(clientsocket, address):   
     print(f"Connection from {address} has been established.")
-    img = util.encodeImage("sample/train.jpg")
-    msg = util.objTobytes(img)
-    clientsocket.send(msg)
-    
+    connected = True
+    while connected:
+        # img = util.encodeBytes("/Users/hangruicao/acai/emulator/YOLOv3_Train_Inference/yolo_detector.pt")
+        # msg = util.imgTobytes(img)
+        # print("the length of message", len(msg))
+        # clientsocket.send(msg)
+        torchtensor = torch.rand(2,3)
+        pobj = pickle.dumps(torchtensor)
+        msg = util.imgTobytes(pobj)
+        clientsocket.sendall(msg)
+        print("message sent")
 
-    
+        while True:
+            # receive the ACK 
+            new_msg = False
+            msg = s.recv(16)
+            if new_msg:
+                print("new msg len:",msg[:HEADERSIZE])
+                msglen = int(msg[:HEADERSIZE])
+                new_msg = False
+
+        
+
+
+if __name__ == "__main__":
+    print("[STARTING] Server wakes up...")
+    HEADERSIZE = 10
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((socket.gethostname(), 1234))
+    s.listen(5)
+    clientsocket = None
+    while True:
+    # now our endpoint knows about the OTHER endpoint.
+        clientsocket, address = s.accept()
+
+        handle_client(clientsocket, address)
+        # clientsocket.close()

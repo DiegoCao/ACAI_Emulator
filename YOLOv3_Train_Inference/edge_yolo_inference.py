a42b231d46bcd592d5bb3b5021a2a8bdd588c5dc
Qiu Yuqing <qyuqing@umich.edu>
2023-04-29 19:50:50 -0400

    add accuracy

--- YOLOv3_Train_Inference/edge_yolo_inference.py
+++ YOLOv3_Train_Inference/edge_yolo_inference.py
@@ -21,17 +21,17 @@ gt_dir = 'mAP/input/ground-truth'
 model_pretrained_path = 'yolo_pretrained_detector.pt'
 model_updated_path = 'yolo_updated_edge_detector.pt'
 
+val_dataset = get_pascal_voc2007_data('content', 'val')
+inference_dataset = filter_dataset_with_class(val_dataset, 'cat', 1)
+
 send_buffer = []
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 s.settimeout(10.0)
 
-def get_data_loader():
-    # get dataset
-    val_dataset = get_pascal_voc2007_data('content', 'val')
-    inference_dataset = filter_dataset_with_class(val_dataset, 'cat', 1)
-    inference_loader = pascal_voc2007_loader(inference_dataset, 1)
+def get_data_loader(batch_size):
+    inference_loader = pascal_voc2007_loader(inference_dataset, batch_size)
     return inference_loader
 
 def check_predictions(boxes, valid_box, resized_proposals, idx, images, w_batch, h_batch, img_ids):
@@ -40,7 +40,8 @@ def check_predictions(boxes, valid_box, resized_proposals, idx, images, w_batch,
     gt_classes = [idx_to_class[b[4].item()] for b in resized_proposals]
     gt_classes.sort()
     if detected_classes != gt_classes:
-        send_buffer.append([images[idx], boxes[idx], w_batch[idx], h_batch[idx], img_ids[idx]])
+        return False
+    return True
 
 def prepare_send_samples():
     global send_buffer
@@ -67,23 +68,55 @@ def prepare_send_samples():
     w_batch = torch.tensor(w_batch_lis)
     h_batch = torch.tensor(h_batch_lis)
 
-    # print("img batch shape: ", image_batch.shape)
-    # print("box batch shape: ", box_batch.shape)
-    # print("w_batch shape: ", w_batch.shape)
-    # print("h_batch shape: ", h_batch.shape)
-    # print("img_id length: ", len(img_id_list))
+    # aggregate to one tuple
     send_samples = (image_batch, box_batch, w_batch, h_batch, img_id_list)
     send_buffer = []
     return send_samples
 
 def update_model():
     send_samples = prepare_send_samples()
+    print(send_samples[0].shape)
     bytes_send = tensorToMessage(send_samples)
     s.sendall(bytes_send)  # send all bytes 
     receive_weights(s, model_updated_path)
     new_detector = SingleStageDetector()
     new_detector.load_state_dict(torch.load(model_updated_path, map_location=torch.device('cpu')))
+    new_detector.eval()
+    print("INFO: updated model accuracy is ", get_accuracy(new_detector))
     return new_detector
+
+def get_accuracy(detector):
+    # get data loader
+    batch_size = 1
+    inference_loader = get_data_loader(batch_size)
+
+    total = 0
+    correct = 0
+
+    # start inference
+    for _, data_batch in enumerate(inference_loader):
+        images, boxes, w_batch, h_batch, img_ids = data_batch
+        final_proposals, final_conf_scores, final_class = detector.inference(images, thresh=thresh, nms_thresh=nms_thresh)
+
+        # clamp on the proposal coordinates
+        for idx in range(batch_size):
+            torch.clamp_(final_proposals[idx][:, 0::2], min=0, max=w_batch[idx])
+            torch.clamp_(final_proposals[idx][:, 1::2], min=0, max=h_batch[idx])
+            valid_box = sum([1 if j != -1 else 0 for j in boxes[idx][:, 0]])
+            final_all = torch.cat((final_proposals[idx], \
+                                final_class[idx].float(), final_conf_scores[idx]), dim=-1).cpu()
+            resized_proposals = coord_trans(final_all, w_batch[idx], h_batch[idx])
+
+            # check if the predictions are incorrect
+            rst = check_predictions(boxes, valid_box, resized_proposals, idx, images, w_batch, h_batch, img_ids)
+
+            if rst:
+                correct += 1
+            total += 1
+    
+    # calculate accuracy
+    accuracy = (correct / total) * 100
+    return accuracy
 
 def inference():
     if os.path.exists(det_dir):
@@ -94,24 +127,25 @@ def inference():
     os.mkdir(gt_dir)
 
     # get data loader
-    inference_loader = get_data_loader()
+    batch_size = 1
+    inference_loader = get_data_loader(batch_size)
 
     # load pre-trained model
     detector = SingleStageDetector()
     detector.load_state_dict(torch.load(model_pretrained_path, map_location=torch.device('cpu')))
     detector.eval()
 
+    print("INFO: pretrained accuracy is ", get_accuracy(detector))
+
     # start inference
-    for iter_num, data_batch in enumerate(inference_loader):
+    for _, data_batch in enumerate(inference_loader):
         images, boxes, w_batch, h_batch, img_ids = data_batch
         final_proposals, final_conf_scores, final_class = detector.inference(images, thresh=thresh, nms_thresh=nms_thresh)
 
         # clamp on the proposal coordinates
-        batch_size = len(images)
         for idx in range(batch_size):
             torch.clamp_(final_proposals[idx][:, 0::2], min=0, max=w_batch[idx])
             torch.clamp_(final_proposals[idx][:, 1::2], min=0, max=h_batch[idx])
-            img = images[idx]
             valid_box = sum([1 if j != -1 else 0 for j in boxes[idx][:, 0]])
             final_all = torch.cat((final_proposals[idx], \
                                 final_class[idx].float(), final_conf_scores[idx]), dim=-1).cpu()
@@ -127,17 +161,17 @@ def inference():
                 for b in resized_proposals:
                     f_det.write('{} {:.6f} {:.2f} {:.2f} {:.2f} {:.2f}\n'.format(idx_to_class[b[4].item()], b[5], b[0], b[1], b[2], b[3]))
 
-        # check if the predictions are incorrect
-        check_predictions(boxes, valid_box, resized_proposals, idx, images, w_batch, h_batch, img_ids)
+            # check if the predictions are incorrect
+            if not check_predictions(boxes, valid_box, resized_proposals, idx, images, w_batch, h_batch, img_ids):
+                send_buffer.append([images[idx], boxes[idx], w_batch[idx], h_batch[idx], img_ids[idx]])
 
-        # communicate with cloud to get new model checkpoint
-        if len(send_buffer) == incorrect_thresh:
-            print("INFO: Reach threshold, need to communicate with cloud!")
-            detector = update_model()
-            print("INFO: Update model at edge!")
-        # sleep between inference requests
-        time.sleep(random.randint(1, 10) / 10)
-
+            # communicate with cloud to get new model checkpoint
+            if len(send_buffer) == incorrect_thresh:
+                print("INFO: Reach threshold, need to communicate with cloud!")
+                detector = update_model()
+                print("INFO: Update model at edge!")
+            # sleep between inference requests
+        # time.sleep(random.randint(1, 10) / 10)
 
 if __name__ == "__main__":
     inference()
